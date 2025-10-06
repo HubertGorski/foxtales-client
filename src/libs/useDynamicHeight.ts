9ace5903cbb616458cd32bc2d05b44a0d318d2a3
hubert <hubert.gorski@bowwe.com>
2025-10-07 00:22:13 +0200

    test timeout

--- src/libs/useDynamicHeight.ts
+++ src/libs/useDynamicHeight.ts
@@ -2,16 +2,24 @@ import { onMounted, onBeforeUnmount } from "vue";
 
 export function useDynamicHeight(selector: string) {
   let element: HTMLElement | null = null;
+  let resizeTimeout: number | null = null;
 
   const adjustHeight = () => {
     if (!element) return;
+
     const height = window.visualViewport
       ? window.visualViewport.height
       : window.innerHeight;
+
     element.style.height = `${height}px`;
   };
 
-  const onResize = () => adjustHeight();
+  const onResize = () => {
+    if (resizeTimeout) clearTimeout(resizeTimeout);
+    resizeTimeout = window.setTimeout(() => {
+      adjustHeight();
+    }, 100);
+  };
 
   onMounted(() => {
     element = document.querySelector(selector);
@@ -27,6 +35,8 @@ export function useDynamicHeight(selector: string) {
   });
 
   onBeforeUnmount(() => {
+    if (resizeTimeout) clearTimeout(resizeTimeout);
+
     if (window.visualViewport) {
       window.visualViewport.removeEventListener("resize", onResize);
     } else {
