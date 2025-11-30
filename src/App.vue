6c08cf3caff7780f4f545dcc988461e37b0e7e0a
hubert <hubert.gorski@bowwe.com>
2025-11-30 21:13:42 +0100

    quick fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router';
   import { useDynamicHeight } from './libs/useDynamicHeight';
-  import { useKeyboardScrollControl } from './libs/useKeyboardScrollControl';
+  // import { useKeyboardScrollControl } from './libs/useKeyboardScrollControl';
   import { useOrientation } from './libs/useOrientation';
   import OrientationOverlay from './components/OrientationOverlay.vue';
   import { useDevice } from './libs/useDevice';
@@ -9,7 +9,7 @@
   useDynamicHeight('#app');
   useDynamicHeight('.foxTales');
   useDevice();
-  useKeyboardScrollControl();
+  // useKeyboardScrollControl();
 
   const { isLandscape } = useOrientation();
 </script>
