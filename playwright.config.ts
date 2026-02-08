28a54a1351fe8dc14e02c57de3f2980568ee3d5f
hubert <hubert.gorski@bowwe.com>
2026-02-08 19:30:52 +0100

    fix custom catalog in create game panel

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,12 +23,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  use: {
+    baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
   },
 
@@ -39,15 +36,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,45 @@
 import { test, expect } from '@playwright/test';
+// npx playwright codegen http://localhost:5173
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('health endpoint works', async ({ request }) => {
+  const response = await request.get('/health');
+  expect(response.ok()).toBeTruthy();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('create tmp account | create question | delete question', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  const tryButton = page.getByText(/Try without an account/i);
+  await expect(tryButton).toBeVisible();
+  await tryButton.click();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.getByLabel('').click();
+  await page.getByLabel('').fill('Hubert');
+  await page.locator('#checkbox-2').check();
+  await page.getByText('Wypróbuj bez konta').click();
+  await page.getByText('Hubert!').click();
+  await page
+    .locator('div')
+    .filter({ hasText: /^Zarządzaj biblioteką$/ })
+    .nth(2)
+    .click();
+  await page.getByPlaceholder('Gdyby **** przez jeden dzień').click();
+  await page
+    .getByPlaceholder('Gdyby **** przez jeden dzień')
+    .fill('Co by było gdyby **** zdobył mlion?');
+  await page
+    .locator('div')
+    .filter({ hasText: /^Dodaj$/ })
+    .nth(2)
+    .click();
+  await page.getByText('Co by było gdyby **** zdobył').click();
+  await page
+    .locator('div')
+    .filter({ hasText: /^Dodaj pytanie$/ })
+    .click();
+  await page.locator('.items').click();
+  await page.getByText('Co by było gdyby **** zdobył').click();
+  await page.getByText('Co by było gdyby **** zdobył').click();
+  await page.getByText('Usuń').click();
+  await page.getByText('Zatwierdź').nth(2).click();
+  await page.getByRole('img', { name: 'Lisek' }).nth(5).click();
 });
