2b95697d82b1b75328c361cea6a29c76e21ee691
hubert <hubert.gorski@bowwe.com>
2026-02-20 17:45:18 +0100

    add admin service and client

---
+++ src/api/services/AdminService.ts
@@ -0,0 +1,7 @@
+import { adminClient } from "../clients/AdminClient";
+
+export const adminService = {
+  async restoreBackup(index: number): Promise<{ data: string }> {
+    return await adminClient.restoreBackup(index);
+  },
+};
