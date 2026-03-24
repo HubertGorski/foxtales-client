388aa970a0cf0b53c5444d3c2a956008baf80b63
hubert <hubert.gorski@bowwe.com>
2026-03-24 15:19:20 +0100

    add middleware

---
+++ middleware.js
@@ -0,0 +1,22 @@
+import { next } from '@vercel/edge';
+
+export const config = {
+  matcher: ['/share/memory/:path*', '/api/:path*', '/psychhub/:path*'],
+};
+
+export default function middleware(request) {
+  const apiKey = process.env.VERCEL_API_KEY;
+
+  if (apiKey) {
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set('X-API-Key', apiKey);
+
+    return next({
+      request: {
+        headers: requestHeaders,
+      },
+    });
+  }
+
+  return next();
+}
