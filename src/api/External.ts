6a6285a07891d443de3b7d2ce44bed418112ece1
hubert <hubert.gorski@bowwe.com>
2026-03-25 11:49:02 +0100

    quick fix

--- src/api/External.ts
+++ src/api/External.ts
@@ -11,6 +11,13 @@ export const platforms: IPlatform[] = [
     color: '#0099ff',
     icon: `<svg viewBox="0 0 24 24" width="20" height="20" fill="#0099ff"><path d="M12 0C5.373 0 0 4.974 0 11.111c0 3.498 1.744 6.614 4.469 8.652V24l4.088-2.242c1.092.3 2.246.464 3.443.464 6.627 0 12-4.974 12-11.111S18.627 0 12 0zm1.191 14.963l-3.055-3.26-5.963 3.26L10.732 8l3.131 3.259L19.752 8l-6.561 6.963z"/></svg>`,
     getUrl: (url: string) =>
+      `https://www.facebook.com/dialog/send?link=${encodeURIComponent(url)}&app_id=${import.meta.env.VITE_META_ID}&redirect_uri=${url}`,
+  },
+  {
+    name: 'Facebook',
+    color: '#1877f2',
+    icon: `<svg viewBox="0 0 24 24" width="20" height="20" fill="#1877f2"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>`,
+    getUrl: (url: string) =>
       `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`,
   },
   {
